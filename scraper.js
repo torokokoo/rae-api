da6b1fd76602d4adef5a17f03b59311ce9bdcce6
Diego <torokokov7@gmail.com>
2020-11-12 12:50:04 -0300

    First complete release

--- scraper.js
+++ scraper.js
@@ -6,7 +6,7 @@ const http = require('http');
 
 module.exports = function (req) {
 
-    fetch(`https://dle.rae.es${req}`)
+    return fetch(`https://dle.rae.es${req}`)
     .then(response => {
         return response.text()
     })
@@ -16,13 +16,18 @@ module.exports = function (req) {
         const dom = new jsdom.JSDOM(res);
 
         var resultados = dom.window.document.getElementById('resultados');
+        // Palabra
         var word = resultados.getElementsByTagName('header')[0].textContent.replace(/[0-9]/g, '');
+
+        // Definicion
         var def = dom.window.document.getElementsByClassName('j');
 
         json = {
             word: word,
             def: def[0].textContent
         }
+
+        return(json)
     })
 
 };
--- server.js
+++ server.js
@@ -2,10 +2,18 @@ const http = require("http");
 const scraper = require("./scraper.js")
 
 const server = http.createServer((req, res) => {
-    if(req.url != '/favicon.ico'){
-        res.setHeader('Content-Type', 'application/json')
-        res.end(JSON.stringify(scraper(req.url)));
-    };    
+    if(req.url == '/'){
+        res.statusCode = 200;
+        res.end("\n\Para utilizar la API por favor ingresa la palabra al lado del dominio, asi: dominio.com/<palabra>\n\
+        Esto te retornara la primera definicion asignada a aquella palabra\n\
+        Para cualquier duda, puedes preguntarme a traves de Discord https://discord.gg/2Tw8aUV o en diego@torokoko.cl");
+    };
+    if(req.url != '/favicon.ico' && req.url != '/'){
+        res.setHeader('Content-Type', 'application/json');
+        res.statusCode = 200;
+        i = scraper(req.url);
+        i.then(response => res.end(JSON.stringify(response)));
+    };
 });
 
 server.listen(8080, 'localhost', () => {
